9890931fff4bd83e2989b5634ac211b62cbd765e
ismail.vega <ismail.vega@deltatre.com>
2021-04-23 19:01:40 -0300

    CFL-NavBar wip

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -3,12 +3,11 @@ import { NavLink } from 'react-router-dom';
 import Logo from '../../images/logos/logo_white.svg';
 
 const ITEMS = [
-  { id: 3, path: '/', value: 'Logo' },
   { id: 1, path: '/games', value: 'Games' },
   { id: 2, path: '/players', value: 'Players' },
-  { id: 4, path: '/standings', value: 'Standings' },
-  { id: 5, path: '/teams', value: 'Teams' },
-  { id: 6, path: '/seasons', value: 'Seasons' },
+  { id: 3, path: '/standings', value: 'Standings' },
+  { id: 4, path: '/teams', value: 'Teams' },
+  { id: 5, path: '/seasons', value: 'Seasons' },
 ];
 
 // handle active item with location path value instead to get the correct value after reload
@@ -16,40 +15,38 @@ function NavBar(props) {
   const [activeItem, setActiveItem] = useState(null);
 
   return (
-    <nav>
-      <div className="nav-wrapper d-flex">
-        <ul className="nav-list flex-centered">
-          {ITEMS.map((item, index) =>
-            item.value !== 'Logo' ? (
-              <li
-                key={item.id}
-                className={`nav-item${
-                  activeItem === item.id ? ' icon-marker' : ''
-                }`}
-                onClick={() => setActiveItem(item.id)}
-              >
-                <NavLink to={item.path}>{item.value}</NavLink>
-              </li>
-            ) : (
-              <li
-                key={item.id}
-                className="brand-logo"
-                onClick={() => setActiveItem(null)}
-              >
-                <NavLink exact to={item.path}>
-                  <img
-                    width="50"
-                    height="50"
-                    src={Logo}
-                    alt="CFL logo"
-                    className="logo"
-                  />
-                </NavLink>
-              </li>
-            )
-          )}
-        </ul>
-      </div>
+    <nav className="cfl-nav d-flex">
+      <button class="cfl-nav__hamburger-icon" aria-label="Navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      <ul className="cfl-nav__list flex-centered">
+        {ITEMS.map((item) => (
+          <li
+            key={item.id}
+            className={`cfl-nav__list-item${
+              activeItem === item.id ? ' icon-marker' : ''
+            }`}
+            onClick={() => setActiveItem(item.id)}
+          >
+            <NavLink to={item.path}>{item.value}</NavLink>
+          </li>
+        ))}
+      </ul>
+
+      <span className="cfl-nav__logo" onClick={() => setActiveItem(null)}>
+        <NavLink exact to="/">
+          <img
+            width="50"
+            height="50"
+            src={Logo}
+            alt="CFL logo"
+            className="logo"
+          />
+        </NavLink>
+      </span>
     </nav>
   );
 }
