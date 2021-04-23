a71136fc500402a1136ed0e88391b95b4d5a04a6
ismail.vega <ismail.vega@deltatre.com>
2021-04-22 21:33:11 -0300

    LFC-Font and logo change - NavBar fixes

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from '../../images/logos/logo_white.svg';
 
-function NavBar() {
-  return <nav>I'm a navigation bar</nav>;
+const ITEMS = [
+  { id: 3, path: '/', value: 'Logo' },
+  { id: 1, path: '/games', value: 'Games' },
+  { id: 2, path: '/players', value: 'Players' },
+  { id: 4, path: '/standings', value: 'Standings' },
+  { id: 5, path: '/teams', value: 'Teams' },
+  { id: 6, path: '/seasons', value: 'Seasons' },
+];
+
+// handle active item with location path value instead to get the correct value after reload
+function NavBar(props) {
+  const [activeItem, setActiveItem] = useState(null);
+
+  return (
+    <nav>
+      <div className="nav-wrapper d-flex">
+        <ul className="nav-list flex-centered">
+          {ITEMS.map((item, index) =>
+            item.value !== 'Logo' ? (
+              <li
+                key={item.id}
+                className={`nav-item${
+                  activeItem === item.id ? ' icon-marker' : ''
+                }`}
+                onClick={() => setActiveItem(item.id)}
+              >
+                <NavLink to={item.path}>{item.value}</NavLink>
+              </li>
+            ) : (
+              <li
+                key={item.id}
+                className="brand-logo"
+                onClick={() => setActiveItem(null)}
+              >
+                <NavLink exact to={item.path}>
+                  <img
+                    width="50"
+                    height="50"
+                    src={Logo}
+                    alt="CFL logo"
+                    className="logo"
+                  />
+                </NavLink>
+              </li>
+            )
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
